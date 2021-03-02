8d3a8153709a8da245e8818d8e46c8eec8c5622b
Jero995 <jeronimo-1995@hotmail.com>
2021-03-01 19:36:53 -0500

    connecting to flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_balcoder/ui/home/home_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   FirebaseApp app = await Firebase.initializeApp();
+    assert(app != null);
+    print('Initialized default app $app');
+
+    runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData( 
         primarySwatch: Colors.blue,
